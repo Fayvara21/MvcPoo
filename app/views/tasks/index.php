e17c135d214c4eaaface6901c8374d019931ffb7
Fayvara21 <thelogane974@gmail.com>
2026-04-03 14:27:13 +0200

    er

--- app/views/tasks/index.php
+++ app/views/tasks/index.php
@@ -191,7 +191,7 @@ $tasks = array_filter($tasks, function($task) use ($activeStates) {
                         </div>
 
                         <div class="small text-muted mb-1">
-                            <div><strong>Désignation:</strong><?= e($a['designation']) ?>
+                            <div><strong>Désignation:</strong><?= e($a['designation']) ?></div>
                         </div>
 
                         <div class="small text-muted d-flex flex-wrap gap-3">
