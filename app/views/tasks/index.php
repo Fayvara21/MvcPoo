746319a0dd8481250669c37efb4cb102ab508585
Fayvara21 <thelogane974@gmail.com>
2026-04-15 17:09:25 +0200

    Add d-inline-flex class to actions cell for improved layout consistency

--- app/views/tasks/index.php
+++ app/views/tasks/index.php
@@ -220,7 +220,7 @@ $tasks = array_filter($tasks, function ($task) use ($activeStates) {
                                 <?= e($task["user_name"]) ?>
                             </td>
 
-                            <td class="actions">
+                            <td class="actions d-inline-flex">
                                 <div class="d-inline-flex flex-wrap">
 
                                     <?php if ($canEdit): ?>
