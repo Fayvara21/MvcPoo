c714dde692c5842569b26c377990d9c6e00dad00
Fayvara21 <thelogane974@gmail.com>
2026-04-20 17:07:01 +0200

    Refactor task creation scripts: move to create directory and update paths

--- app/views/tasks/create.php
+++ app/views/tasks/create.php
@@ -92,8 +92,8 @@
 
 </div>
 
-<script defer src="/js/tasks/appro.js"></script>
-<script defer src="/js/tasks/retour.js"></script>
-<script defer src="/js/tasks/task-type-toggle.js"></script>
+<script defer src="/js/tasks/create/appro.js"></script>
+<script defer src="/js/tasks/create/retour.js"></script>
+<script defer src="/js/tasks/create/task-type-toggle.js"></script>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
