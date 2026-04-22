1c4584c13cee591c7d4c69a02da2ccaea836d7d5
Fayvara21 <thelogane974@gmail.com>
2026-04-22 14:18:26 +0200

    Refactor APPRO and RETOUR partials: enhance structure and add placeholders for inputs

--- app/views/tasks/partials/appro.php
+++ app/views/tasks/partials/appro.php
@@ -10,31 +10,32 @@
             + Ajouter PN
         </button>
 
-        <div id="approList">
-            <div class="border rounded p-2 mb-2">
-                <div class="row g-2">
-                    <div class="col">
-                        <input class="form-control" name="appro[0][pn]" required>
-                    </div>
-                    <div class="col">
-                        <input class="form-control" name="appro[0][location]">
-                    </div>
-                    <div class="col">
-                        <input class="form-control" type="number" name="appro[0][nb]" value="1">
-                    </div>
-                    <div class="col-auto">
-                        <button type="button" class="btn btn-danger remove">✕</button>
+        <div id="approFields" class="card shadow-sm border-0 mb-3" style="display:none;">
+            <div class="card-body">
+                <div class="d-flex align-items-center gap-2 mb-3"> <span class="badge bg-primary">APPRO</span>
+                    <h5 class="fw-semibold mb-0">Informations APPRO</h5>
+                </div> <!-- MULTI ROW --> <button type="button" id="addAppro" class="btn btn-sm btn-primary mb-3"> +
+                    Ajouter PN </button>
+                <div id="approList">
+                    <div class="border rounded p-2 mb-2">
+                        <div class="row g-2">
+                            <div class="col"> <input class="form-control" name="appro[0][pn]" placeholder="PN" required>
+                            </div>
+                            <div class="col"> <input class="form-control" name="appro[0][location]"
+                                    placeholder="Emplacement"> </div>
+                            <div class="col"> <input class="form-control" type="number" name="appro[0][nb]" value="1">
+                            </div>
+                            <div class="col-auto"> <button type="button" class="btn btn-danger remove">✕</button> </div>
+                        </div>
                     </div>
                 </div>
+
+                <hr>
+
+                <input class="form-control mb-2" name="appro_designation">
+                <input class="form-control mb-2" name="appro_of" required>
+                <input class="form-control mb-2" name="appro_plane">
+                <input class="form-control mb-2" name="appro_oe">
+
             </div>
         </div>
-
-        <hr>
-
-        <input class="form-control mb-2" name="appro_designation">
-        <input class="form-control mb-2" name="appro_of" required>
-        <input class="form-control mb-2" name="appro_plane">
-        <input class="form-control mb-2" name="appro_oe">
-
-    </div>
-</div>
--- app/views/tasks/partials/retour.php
+++ app/views/tasks/partials/retour.php
@@ -10,26 +10,29 @@
             + Ajouter PN
         </button>
 
-        <div id="retourList">
-            <div class="border rounded p-2 mb-2">
-                <div class="row g-2">
-                    <div class="col">
-                        <input class="form-control" name="retour[0][pn]" required>
-                    </div>
-                    <div class="col">
-                        <input class="form-control" type="number" name="retour[0][nb]" value="1">
-                    </div>
-                    <div class="col-auto">
-                        <button type="button" class="btn btn-danger remove">✕</button>
+        <div id="retourFields" class="card shadow-sm border-0" style="display:none;">
+            <div class="card-body">
+                <div class="d-flex align-items-center gap-2 mb-3"> <span
+                        class="badge bg-warning text-dark">RETOUR</span>
+                    <h5 class="fw-semibold mb-0">Informations RETOUR</h5>
+                </div> <button type="button" id="addRetour" class="btn btn-sm btn-warning mb-3"> + Ajouter PN </button>
+                <!-- MULTI ROW -->
+                <div id="retourList">
+                    <div class="border rounded p-2 mb-2">
+                        <div class="row g-2">
+                            <div class="col"> <input class="form-control" name="retour[0][pn]" placeholder="PN"
+                                    required> </div>
+                            <div class="col"> <input class="form-control" type="number" name="retour[0][nb]" value="1">
+                            </div>
+                            <div class="col-auto"> <button type="button" class="btn btn-danger remove">✕</button> </div>
+                        </div>
                     </div>
                 </div>
+
+                <hr>
+
+                <input class="form-control mb-2" name="retour_sn">
+                <input class="form-control mb-2" name="retour_certif">
+
             </div>
         </div>
-
-        <hr>
-
-        <input class="form-control mb-2" name="retour_sn">
-        <input class="form-control mb-2" name="retour_certif">
-
-    </div>
-</div>
