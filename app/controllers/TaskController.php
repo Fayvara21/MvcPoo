3b7c2d0103ab0e4db5d74945f9a4c2919f754e70
Fayvara21 <thelogane974@gmail.com>
2026-04-27 12:23:38 +0200

    Add debug output for verif_stock POST data in TaskController

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -94,6 +94,8 @@ class TaskController extends BaseController
 
             // === VERIF STOCK ===
             if ($type === 'verif_stock' && !empty($_POST['verif_stock'])) {
+
+                var_dump($_POST);
                 $verifStockRows = $_POST['verif_stock'];
 
                 $sharedLocation = $_POST['verif_stock_location'] ?? null;
