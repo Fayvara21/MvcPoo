a1dc3db50fdb6037942b312a9c7449f5c79aac55
Fayvara21 <thelogane974@gmail.com>
2026-04-29 14:08:07 +0200

    feat: add update log for pagination implementation and search logic modifications

--- app/views/projects/index.php
+++ app/views/projects/index.php
@@ -159,6 +159,17 @@
     <div class=" CHANGELOG d-flex justify-content-end mt-4">
         <div class="container my-4 overflow-y-auto" style="max-height: 500px;">
 
+        <div class="mb-3 p-4 border rounded shadow-sm bg-light">
+                <h4 class="mb-2 text-primary">Mise à jour v260428:</h4>
+
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Implémentation de la pagination des demandes</li>
+                    <li class="lis-t-group-item">Modification de la logique de recherche:</li>
+                    <li class="list-group-item">- La recherche se fait désormais dans tous les champs (titre, description, PN, OF, etc.)</li>
+                    <li class="list-group-item">- Les résultats de la recherche sont compatibles avec les filtres</li>
+                </ul>
+            </div>
+
             <div class="mb-3 p-4 border rounded shadow-sm bg-light">
                 <h4 class="mb-2 text-primary">Mise à jour v260428:</h4>
 
